9704e9d1d4460dac01fc9c4b9c0e391e7ac61211
AlaaAhmed2221 <allakhamis200@gmail.com>
2026-08-30 02:08:48 +0300

    Create database 'H_DWH' with initial schemas
    Create a new database 'H_DWH' with schemas bronze, silver, and gold.

--- Script/initial_database.sql
+++ Script/initial_database.sql
@@ -1,1 +1,20 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+create a new DB named 'H_DWH'
+*/
+
+create database H_DWH;
+
+use H_DWH
+Go
+
+--Create Schemas
+
+create schema bronze;
+GO
+create schema silver;
+GO
+create schema gold;
 
