3c18ffccf9573551da22a18e553039ed1ae3e5f3
AlaaAhmed2221 <allakhamis200@gmail.com>
2026-08-31 23:06:01 +0300

    Add dwh_create_date column to SQL tables
    Added dwh_create_date column with default value to multiple tables.

--- Script/Silver/ddl.silver.sql
+++ Script/Silver/ddl.silver.sql
@@ -17,9 +17,10 @@ IF OBJECT_ID('silver.appointments', 'U') IS NOT NULL
 	patient_id       varchar(50),
     doctor_id        varchar(50),
 	appointment_date date,
-	appointment_time time,
+	appointment_time time(0),
 	reason_for_visit varchar(50),
-    status varchar(50)
+    status varchar(50),
+	 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 	)
 Go
 
@@ -33,7 +34,8 @@ IF OBJECT_ID('silver.billing', 'U') IS NOT NULL
 	bill_date      date,
     amount         float,
     payment_method varchar(50),
-	payment_status varchar(50)
+	payment_status varchar(50),
+	 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 		)
 Go
 
@@ -48,7 +50,8 @@ IF OBJECT_ID('silver.doctors', 'U') IS NOT NULL
 	phone_number     varchar(50),
 	years_experience int,
     hospital_branch  varchar(50),
-	email            varchar(50)
+	email            varchar(50),
+	 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 		)
 Go
 
@@ -66,7 +69,8 @@ IF OBJECT_ID('silver.patients', 'U') IS NOT NULL
 	registration_date date,
 	insurance_provider varchar(50),
 	insurance_number  varchar(50),
-	email             varchar(50)
+	email             varchar(50),
+	 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 		)
 Go
 
@@ -79,5 +83,6 @@ IF OBJECT_ID('silver.treatments', 'U') IS NOT NULL
 	treatment_type  varchar(50),
 	description     varchar(50),
 	cost            float,
-	treatment_date  date
+	treatment_date  date,
+	 dwh_create_date    DATETIME2 DEFAULT GETDATE()
 		)
